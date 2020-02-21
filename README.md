61058b728ea18601811173bdd2d0d992c53b24bf
Paul Hammant <paul@hammant.org>
2020-02-21 10:51:08 +0000

    authors, license

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Climate data api app
+# Climate Data demo app for Servirtium-Dart 
+
+Step 1 of https://github.com/servirtium/README/blob/master/starting-a-new-implementation.md complete
+
+Step 2 next..
--- test/climate_api_test.dart
+++ test/climate_api_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2020, the Servirtium project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 import 'package:climate_data_api/climate_api.dart';
 import 'package:test/test.dart';
 
